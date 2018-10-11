3324abd49628d1220ad68bdb91c98ddfc322f61e
shellyln <shellyln@users.noreply.github.com>
2018-10-11 18:49:29 +0900

    Add exported symbols.

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,9 @@
 
 
 export * from './s-exp/s-expression';
+
+export { default as installCore }       from './s-exp/operators/core';
+export { default as installArithmetic } from './s-exp/operators/arithmetic';
+export { default as installSequence }   from './s-exp/operators/sequence';
+export { default as installJsx }        from './s-exp/operators/jsx';
+export { default as installConcurrent } from './s-exp/operators/concurrent';

e055c32ac2730c7331ba05d3b6541a6eeac17da4
shellyln <shellyln@users.noreply.github.com>
2020-09-11 21:18:35 +0900

    Fix spec code

--- src/_spec/fixes.spec.ts
+++ src/_spec/fixes.spec.ts
@@ -6,7 +6,9 @@ import { globalObj, objConstructor, funConstructor } from '../s-exp/global-this'
 describe("global-this", function() {
     it("global-this", function() {
         expect(globalObj === global).toEqual(true);
-        expect(globalObj === globalThis).toEqual(true);
+        if (typeof globalThis === 'object') {
+            expect(globalObj === globalThis).toEqual(true);
+        }
         expect(objConstructor === Object).toEqual(true);
         expect(funConstructor === Function).toEqual(true);
     });
